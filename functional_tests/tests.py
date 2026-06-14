f2b3d674459709e9fe15e66abca4b713529dc6d1
RosATypeN <1498369197@qq.com>
2026-06-14 15:18:05 +0800

    message 20: Add Bootstrap framework, implement template inheritance and layout tests passed

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -101,3 +101,27 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element(By.TAG_NAME, 'body').text
         self.assertNotIn('Buy flowers', page_text)
         self.assertIn('Buy milk', page_text)
+
+    def test_layout_and_styling(self):
+        # 张三访问首页 
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # 看到输入框完美居中对齐 
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # 他新建了一个清单，看到在列表页输入框也是完美居中的 
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
